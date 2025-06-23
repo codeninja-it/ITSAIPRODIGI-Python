2074287f579ad2f796a91e8a6839f99a7f993a7f
Daniele Simoncini <simoncini@codeninja.it>
2025-06-23 10:43:06 +0200

    inizio banca dati tramite sqlite

---
+++ bancadati.py
@@ -0,0 +1,33 @@
+import sqlite3
+from tkinter import simpledialog
+
+testo = simpledialog.askstring("nome del db", "dove vuoi salvare il db?")
+
+# mi connetto alla banca dati
+dbcon = sqlite3.connect("miodb.sqlite")
+
+# ottengo un telefono
+dbexe = dbcon.cursor()
+
+dbexe.execute(
+    "CREATE TABLE IF NOT EXISTS letture("+ 
+        "idLettura INTEGER PRIMARY KEY AUTOINCREMENT," +
+        "velocita NUMBER," + 
+        "temperatura NUMBER," +
+        "umidita NUMBER,"  +
+        "quando DATETIME" +
+    ")"
+)
+
+# per dieci volte
+for n in range(10):
+    # eseguo una richiesta di inserimento
+    dbexe.execute("INSERT INTO letture (velocita, temperatura, umidita) VALUES (0, 0, 0)")
+
+# chiudo la transazione rendendo le mi richieste
+# accessibili anche ad altri
+dbcon.commit()
+
+# chiudo la connessione
+# per risparmiare risorse
+dbcon.close()
