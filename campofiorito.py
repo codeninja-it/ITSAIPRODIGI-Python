95aad1e826523a7e47f9e48090a9f5e598d1ecfc
Daniele Simoncini <simoncini@codeninja.it>
2025-06-25 14:10:20 +0200

    traccia esercizio python

---
+++ campofiorito.py
@@ -0,0 +1,31 @@
+import random
+import time
+
+# chiedo quante caselle di lato
+print("di quante caselle?")
+caselle = int( input() )
+
+# chiedo quante bombe piazzare
+print("quante bombe?")
+bombe = int( input() )
+
+# tengo il conto di quante ne ho piazzate
+piazzate = 0
+# e di quante ne ho trovate
+trovate = 0
+
+# dopo aver inizializzato la griglia in modo randomico
+pass
+
+#fino a che il tempo non è finito
+secondi = 30
+trascorsi = 0
+while(trascorsi < secondi):
+    time.sleep(1)
+    # chiedo le due coordinate
+    pass
+    # controllo se ho trovato una bomba oppure no
+    pass
+
+# quando il tempo è finito se non ho trovato tutte le bombe
+# sono morto altrimenti ho vinto
